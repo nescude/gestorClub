0f817ef14c276d0e1f2a6f6b60fa655a3f5b48b8
TomasAusinoPalau <67170157+TomasAusinoPalau@users.noreply.github.com>
2021-12-09 17:52:38 -0300

    Revert "Implemented MemberServices methods"

--- clubapi/src/main/java/com/example/clubapi/services/MemberService.java
+++ clubapi/src/main/java/com/example/clubapi/services/MemberService.java
@@ -1,11 +1,7 @@
 package com.example.clubapi.Services;
 
-import com.example.clubapi.mock_interfaces.MockMember;
-import com.example.clubapi.mock_interfaces.MockSeat;
 import com.example.clubapi.models.Member;
-import com.example.clubapi.models.Seat;
 import com.example.clubapi.repositories.Member.MemberRepo;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +10,6 @@ public class MemberService {
     
     @Autowired
     MemberRepo repo;
-
-    @Autowired
-    MockSeat service;
 
     public Iterable<Member> getAllMembers(){
         return repo.getAllMembers();
@@ -30,24 +23,8 @@ public class MemberService {
         return repo.save(member) != null;
     }
 
-    public Boolean deleteMember(String dni){
+    public Boolean deleteById(String dni){
         return repo.deleteById(dni);
-    }
-
-    public Seat getClaimedSeat(String dni){
-        return service.findByMemberId();
-    }
-
-    public Iterable<Member> getActiveMembers(){
-        return repo.getAllActiveMembers();
-    }
-
-    public Iterable<Member> getPremiumMembers(){
-        return null;
-    }
-
-    public Iterable<Member> getLifeMembers(){
-        return null;
     }
 
 }
