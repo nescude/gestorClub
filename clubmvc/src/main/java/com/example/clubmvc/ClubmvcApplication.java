8fd2875daab0d9a6315fd9f769f593a6d442e1fa
Tomi <tausinopalau@gmail.com>
2021-12-09 10:43:46 -0300

    mvc microservice

---
+++ clubmvc/src/main/java/com/example/clubmvc/ClubmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.clubmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubmvcApplication.class, args);
+	}
+
+}
---
+++ clubmvc/src/test/java/com/example/clubmvc/ClubmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clubmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
