ee2cec75282dae724d5fd4c83d17476b4b7e6c64
Tomi <tausinopalau@gmail.com>
2021-12-09 09:59:01 -0300

    first commit

---
+++ clubapi/src/main/java/com/example/clubapi/ClubapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.clubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubapiApplication.class, args);
+	}
+
+}
---
+++ clubapi/src/test/java/com/example/clubapi/ClubapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
